e7417e57e7a9d1e845c06335635a050cff0062ae
Tushar Shaw <146598677+Tushar-0202-Shaw@users.noreply.github.com>
2023-11-09 21:06:06 +0530

    Add files via upload

---
+++ Lexicographical.java
@@ -0,0 +1,30 @@
+package Sorting;
+import java.util.Scanner;
+public class Lexicographical {
+    static void swap(String[]arr,int i,int j){
+        String temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array :-");
+        int n = sc.nextInt();
+        String[]arr = new String[n];
+        System.out.println("Enter "+n+" string :-");
+        for(int i=0;i<n;i++){
+            arr[i] = sc.next();
+        }
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if((arr[i].compareTo(arr[j]))>0){
+                    swap(arr,i,j);
+                }
+            }
+        }
+        System.out.println("Sorted order :-");
+        for(String element : arr){
+            System.out.print(element+" ");
+        }
+    }
+}

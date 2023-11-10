8ad3322be0c742b56d2f2447d03140120a7d0c13
Tushar Shaw <146598677+Tushar-0202-Shaw@users.noreply.github.com>
2023-11-10 11:52:42 +0530

    Insertion Sort

---
+++ Insertion_Sort.java
@@ -0,0 +1,30 @@
+package Sorting;
+import java.util.Scanner;
+public class Insertion_Sort {
+    static void swap(int[]arr,int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array :-");
+        int n = sc.nextInt();
+        int[]arr = new int[n];
+        System.out.println("Enter "+n+" elements :-");
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i=1;i<n;i++){
+            int j=i;
+            while(j>0 && arr[j]<arr[j-1]){
+                swap(arr,j,j-1);
+                j--;
+            }
+        }
+        System.out.println("Sorted array :-");
+        for(int element : arr){
+            System.out.print(element+" ");
+        }
+    }
+}
